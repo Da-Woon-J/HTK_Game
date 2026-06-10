7da3481909e2f29e527472988aa155d1d9870594
DW <torry6748@gmail.com>
2026-06-10 15:11:03 +0900

    fix : hand movement range

--- Python/main.py
+++ Python/main.py
@@ -3,11 +3,19 @@ from cvzone.HandTrackingModule import HandDetector
 import udp_socket
 import gesture
 import normalization
+from stabilizer import VectorStabilizer  # 1. 스테빌라이저 클래스 임포트
+
 
 cap = cv2.VideoCapture(0)
 
-# htk 초기화
 detector = HandDetector(maxHands=2, detectionCon=0.8)
+
+stabilizers = {
+    'Left': VectorStabilizer(threshold=3.0, smoothing=0.6),
+    'Right': VectorStabilizer(threshold=3.0, smoothing=0.6)
+}
+
+GAIN = 1.8 
 
 while True:
     success, img = cap.read()
@@ -17,26 +25,28 @@ while True:
         print("캠 신호 x, 연결을 확인하세요.")
         break
 
+    # 캠 화면의 중심점 계산
+    h, w, _ = img.shape
+    cx, cy = w // 2, h // 2
+
     # Hands 감지
     hands, img = detector.findHands(img)
 
     data = []
 
-
     if hands:
-        hand_0 = hands[0]
-        lm_0 = normalization.Normalize(hand_0['lmList'])
-        # lm_0 = hand_0['lmList']
-        type_0 = hand_0['type']
-        gesture_0 = gesture.gestures(lm_0)
-        data.extend([lm_0,type_0,gesture_0])
-        if len(hands) == 2:
-            hand_1 = hands[1]
-            lm_1 = normalization.Normalize(hand_1['lmList'])
-            # lm_1 = hand_1['lmList']
-            type_1 = hand_1['type']
-            gesture_1 = gesture.gestures(lm_1)
-            data.extend([lm_1,type_1,gesture_1])
+        for hand in hands:
+            hand_type = hand['type']  #'Left', 'Right'
+            lm_normalized = normalization.Normalize(hand['lmList'])
+            lm_stabilized = stabilizers[hand_type].update(lm_normalized)
+            hand_gesture = gesture.gestures(lm_stabilized)
+            lm_gained = []
+            for lm in lm_stabilized:
+                gx = cx + (lm[0] - cx) * GAIN
+                gy = cy + (lm[1] - cy) * GAIN
+                gz = lm[2] * GAIN
+                lm_gained.append([gx, gy, gz])
+            data.extend([lm_gained, hand_type, hand_gesture])
 
     print(data)
     udp_socket.send_udp(data)
@@ -47,4 +57,4 @@ while True:
         break
 
 cap.release()
-cv2.destroyAllWindows()      
+cv2.destroyAllWindows()
